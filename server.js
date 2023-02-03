615244291244ab3ae8806b391a567501c13076bd
Garg <Nikhil.Garg@sony.com>
2023-02-03 15:53:01 +0530

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const io = require('socket.io')(8000,{
-    cors:{
-        origin: ['http://localhost:3000']
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: ['http://localhost:3000'],
+        methods: ["GET", "POST"]
     }
 });
 
-io.on('connection', (socket)=>{
-    console.log('Server Connected',socket.id);
-    socket.on('join-room',(roomId, userId)=>{
-        socket.join(roomId);
+var users = Array();
+
+io.on('connection', (socket) => {
+    console.log('connection established', socket.id);
+    socket.emit("me", socket.id)
+
+    socket.on('syncplayers', function(user) {
+        console.log("syncplayers(user)");
+        users = user;
+    });
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+    })
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
     })
 });
